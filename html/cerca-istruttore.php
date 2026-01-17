628fd906f3b93eafdb1dde74014f48940c007584
Okonigiri <207894241+Okonigiri@users.noreply.github.com>
2026-01-17 16:15:20 +0100

    Ricerca istruttore (non so come sia a sicurezza)

--- html/cerca-istruttore.php
+++ html/cerca-istruttore.php
@@ -1,5 +1,50 @@
 <?php
-
 $conn = new mysqli("db", "root", "example", "palestra_db", 3306);
 
+$top = file_get_contents("internal/cerca-istruttore/top.html");
+$templateResult = file_get_contents("internal/cerca-istruttore/stack.html");
+$bottom = file_get_contents("internal/cerca-istruttore/bottom.html");
+
+$nome = '';
+if (isset($_GET['nome'])) {
+    $nome = $_GET['nome'];
+    $nome = trim($nome);
+    $nome = htmlspecialchars($nome);
+}
+
+$top = str_replace("[Valore Ricerca]", $nome, $top);
+
+echo $top;
+
+$query = "SELECT *
+    FROM Utente
+    JOIN Istruttore ON Utente.id_utente = Istruttore.id_utente
+    LEFT JOIN Personal_Trainer ON Utente.id_utente = Personal_Trainer.id_istruttore
+    WHERE nome LIKE ?";
+$stmt = $conn->prepare($query);
+
+$param = "%" . $nome . "%";
+$stmt->bind_param("s", $param);
+
+$stmt->execute();
+
+$result = $stmt->get_result();
+
+if ($result->num_rows == 0) {
+    echo "Nessun risultato trovato.";
+}
+
+while ($row = $result->fetch_array(MYSQLI_ASSOC)) {
+    $output = $templateResult;
+
+    $isPt = $row['id_istruttore'] == null ? '&cross;' : '&check;';
+
+    $output = str_replace("[Nome Istruttore]", $row['nome'], $output);
+    $output = str_replace("[È PT]", $isPt, $output);
+    $output = str_replace("[Specializzazione Istruttore]", $row['specializzazione'], $output);
+    
+    echo $output;
+}
+
+echo $bottom;
 ?>
