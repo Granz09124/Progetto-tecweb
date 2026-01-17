fe530755041630b99c0759646a142689040136f7
Okonigiri <207894241+Okonigiri@users.noreply.github.com>
2026-01-17 16:16:13 +0100

    Aggiunto cognome istruttore

--- html/cerca-istruttore.php
+++ html/cerca-istruttore.php
@@ -39,7 +39,7 @@ while ($row = $result->fetch_array(MYSQLI_ASSOC)) {
 
     $isPt = $row['id_istruttore'] == null ? '&cross;' : '&check;';
 
-    $output = str_replace("[Nome Istruttore]", $row['nome'], $output);
+    $output = str_replace("[Nome Istruttore]", $row['nome'] . ' ' . $row['cognome'], $output);
     $output = str_replace("[È PT]", $isPt, $output);
     $output = str_replace("[Specializzazione Istruttore]", $row['specializzazione'], $output);
     
