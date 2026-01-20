a724e362c31998fe93becf98dd10ec078e3af599
Okonigiri <207894241+Okonigiri@users.noreply.github.com>
2026-01-20 15:44:59 +0100

    Spostata connessione db in sessione

--- html/db_connection.php
+++ html/db_connection.php
@@ -1,4 +1,13 @@
 <?php
+
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
+
+if (isset($_SESSION['db'])) {
+    exit;
+}
+
 $host = "db";
 $user = "root";
 $pass = "example";
@@ -9,3 +18,5 @@ $conn = new mysqli($host, $user, $pass, $db);
 if ($conn->connect_error) {
     die("Connessione fallita: " . $conn->connect_error);
 }
+
+$_SESSION['db'] = $conn;
