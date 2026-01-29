dd0858bd5cb33acb6ecb72ca66b1fe1553559031
marco-beltrame <mcanussio@gmail.com>
2026-01-29 12:00:12 +0100

    andavano insieme al commit di prima...

--- html/header.php
+++ html/header.php
@@ -1,14 +1,19 @@
 <?php
 function renderPage($htmlFile) {
+
+    // Aggiunge se manca / all'inizio del percorso
+    if ($htmlFile[0] !== '/') {
+        $htmlFile = $_SERVER['DOCUMENT_ROOT'] . '/' . $htmlFile;
+    }
     $content = file_get_contents($htmlFile);
     
     $menuFile = isset($_SESSION['user_id'])
-        ? "internal/home/menu-user.html"
-        : "internal/home/menu-guest.html";
-    
+        ? __DIR__ . "/internal/home/menu-user.html"
+        : __DIR__ . "/internal/home/menu-guest.html";
+
     $menuContent = file_get_contents($menuFile);
-    
     $currentPage = pathinfo($_SERVER['PHP_SELF'], PATHINFO_FILENAME);
+    
     
     $pattern = '/<li>\s*<a ([^>]*data-page="' . preg_quote($currentPage, '/') . '"[^>]*)>(.*?)<\/a>\s*<\/li>/';
     
@@ -30,4 +35,33 @@ function renderPage($htmlFile) {
     
     echo $content;
 }
+
+function renderFromHtml($htmlContent) {
+    $menuFile = isset($_SESSION['user_id'])
+        ? $_SERVER['DOCUMENT_ROOT'] . "/internal/home/menu-user.html"
+        : $_SERVER['DOCUMENT_ROOT'] . "/internal/home/menu-guest.html";
+    
+    $menuContent = file_get_contents($menuFile);
+    $currentPage = pathinfo($_SERVER['PHP_SELF'], PATHINFO_FILENAME);
+    
+    $pattern = '/<li>\s*<a ([^>]*data-page="' . preg_quote($currentPage, '/') . '"[^>]*)>(.*?)<\/a>\s*<\/li>/';
+    
+    if (preg_match($pattern, $menuContent, $matches)) {
+        $attributes = $matches[1];
+        $contentInside = $matches[2];
+        
+        $keepLang = '';
+        if (in_array($currentPage, ['home', 'login', 'logout'])) {
+            $keepLang = ' lang="en"';
+        }
+        
+        $span = '<li><span' . $keepLang . ' aria-current="page">' . $contentInside . '</span></li>';
+        $menuContent = preg_replace($pattern, $span, $menuContent);
+    }
+    
+    $htmlContent = str_replace('[Menu]', $menuContent, $htmlContent);
+    
+    echo $htmlContent;
+}
+
 ?>
--- html/internal/utente-semplice.php
+++ html/internal/utente-semplice.php
@@ -1,5 +1,6 @@
 <?php
 require_once 'config.php';
+require "header.php";
 
 // Test Mario Rossi
 $_SESSION['user_id'] = 3; 
@@ -68,6 +69,8 @@ $body = str_replace("[ListaSchede]", $lista, $body);
 $body = str_replace("[Email]", htmlspecialchars($userData['email']), $body);
 $body = str_replace("[Telefono]", htmlspecialchars($userData['telefono']), $body);
 
-echo $top . $body . $bottom;
+renderFromHtml($top . $body . $bottom);
+
+
 $stmtSchede->close(); $conn->close();
 ?>
