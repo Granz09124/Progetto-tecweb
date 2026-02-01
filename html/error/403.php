45f81667086d149dd9332e9472e384581d5e8274
Okonigiri <207894241+Okonigiri@users.noreply.github.com>
2026-02-01 10:11:01 +0100

    Redirect /nome_utente/home.php (bisogna modificare .htaccess.example sostituendo nome_utente e rinominando a .htaccess

--- html/error/403.php
+++ html/error/403.php
@@ -1,11 +1,11 @@
 <?php
 session_start();
-require '../header.php';
+require __DIR__ . '/../../internal/header.php';
 
 // tutte le pagine proprie dell'amministratore restituiscono un 404
 // tutte le pagine utente restituiscono un 403 se non autenticato
 http_response_code(403);
 
-renderPage("internal/error/403.html");
+renderPage(__DIR__ . "/../../internal/error/403.html");
 
 ?>
--- html/error/404.php
+++ html/error/404.php
@@ -1,11 +1,11 @@
 <?php
 session_start();
-require '../header.php';
+require __DIR__ . '/../../internal/header.php';
 
 // tutte le pagine proprie dell'amministratore restituiscono un 404
 // tutte le pagine utente restituiscono un 403 se non autenticato
 http_response_code(404);
 
-renderPage("internal/error/404.html");
+renderPage(__DIR__ . "/../../internal/error/404.html");
 
 ?>
