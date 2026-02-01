4dec88589c16c1df79566b11577b6d17a646f268
Itaracs <130662365+Itaracs@users.noreply.github.com>
2026-02-01 20:36:03 +0100

    Update area-personale.php
    aggiunta sessione

--- html/area-personale.php
+++ html/area-personale.php
@@ -1,8 +1,9 @@
 <?php
+session_start();
 require_once __DIR__ . '/../internal/db_connection.php';
 
 if (!isset($_SESSION['user_id'])) {
-    header("Location: home.php"); 
+    header("Location: login.php"); 
     exit;
 }
 
